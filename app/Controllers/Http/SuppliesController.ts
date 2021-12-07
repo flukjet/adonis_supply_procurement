0b228b366d6f84bff17dd1acf1b0d19c59fa023b
flukjet <flukjet@gmail.com>
2021-12-08 00:30:13 +0700

    Edit Order Cart

--- app/Controllers/Http/SuppliesController.ts
+++ app/Controllers/Http/SuppliesController.ts
@@ -3,29 +3,65 @@ import Product from 'App/Models/Product'
 
 export default class SuppliesController {
 
-    public async index({view}: HttpContextContract) {
+    public async index({request,view}: HttpContextContract) {
+        const order = request.cookie('order',[])
         const products = await Product.all()
-        return view.render('supply',{products: products})
+
+        
+        return view.render('supply',{products: products, order:order})
     }
   
-    public async history({view}: HttpContextContract) {
+    public async history({request,view}: HttpContextContract) {
 
-        return view.render('history')
+        const order = request.cookie('order',[])
+
+        return view.render('history', {order:order})
     }
 
-    public async order({view}: HttpContextContract) {
+    public async order({request,view}: HttpContextContract) {
 
-        return view.render('order')
+        const orders = request.cookie('order',[])
+        const orderDetail = []
+        for(let product of orders){
+          const productdetail = await Product.query()
+                                        .where('id',product.id)
+                                        .first()
+  
+          orderDetail.push({id: product.id,
+                            name: productdetail?.name,
+                            quantity:  product.quantity})
+        }
+
+
+        let holder = {};
+
+        orderDetail.forEach(function(d) {
+          if (holder.hasOwnProperty(d.name)) {
+            holder[d.name] = holder[d.name] + d.quantity;
+          } else {
+            holder[d.name] = d.quantity;
+          }
+        });
+        
+        let orderDetailSort = [];
+        
+        for (let prop in holder) {
+            orderDetailSort.push({ name: prop, quantity: holder[prop] });
+        }
+        
+        
+        return view.render('order', {orders: orderDetailSort, order: orderDetail})
     }
 
     public async orderAdd({request, params, response }:HttpContextContract){
         const order = request.cookie('order',[])
-        const product_id = params.id 
+        const product_id = params.id
   
         const product = { id: product_id, quantity: 1} 
         order.push(product)
         response.cookie('order', order)
-  
+        console.log(product);
+        
         response.redirect().toRoute('home')
     }
 }
